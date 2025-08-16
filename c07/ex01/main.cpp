e1445f0cc1a06e92c4dc8c623de4dfa5a1817786
AMKSA101 <abdelghaniamksa@gmail.com>
2025-08-16 10:59:28 +0100

    changes loading...

--- c07/ex01/main.cpp
+++ c07/ex01/main.cpp
@@ -6,13 +6,53 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 13:10:41 by user              #+#    #+#             */
-/*   Updated: 2025/08/14 13:11:04 by user             ###   ########.fr       */
+/*   Updated: 2025/08/15 10:29:16 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int main() {
-	
+void printInt(int &value)
+{
+	std::cout << value << " ";
+}
+
+void printString(const std::string &value)
+{
+	std::cout << value << " ";
+}
+
+template <typename T>
+void printElement(const T &element)
+{
+	std::cout << element << " ";
+}
+
+void doubleInt(int &value)
+{
+	value *= 2;
+}
+
+int main()
+{
+	// Test 1: Print integers
+	std::cout << "=== Testing int array ===" << std::endl;
+	int intArr[5] = {1, 2, 3, 4, 5};
+	std::cout << "Original: ";
+	iter(intArr, 5, printInt); // Fix: Remove ()
+	std::cout << std::endl;
+
+	// Test 2: Modify integers
+	std::cout << "Doubling values..." << std::endl;
+	iter(intArr, 5, doubleInt);
+	std::cout << "After doubling: ";
+	iter(intArr, 5, printInt);
+	std::cout << std::endl;
+
+	// Test 3: Template function with different types
+	std::cout << "\n=== Testing template function ===" << std::endl;
+	std::string stringArr[2] = {"Amksa", "Abdelghani"};
+	iter(stringArr, 2, printElement<std::string>);
+	std::cout << std::endl;
 	return 0;
 }
--- c07/ex02/Array.hpp
+++ c07/ex02/Array.hpp
@@ -1,25 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/14 13:11:07 by user              #+#    #+#             */
-/*   Updated: 2025/08/15 10:23:26 by user             ###   ########.fr       */
+/*   Created: 2025/08/15 11:17:08 by user              #+#    #+#             */
+/*   Updated: 2025/08/16 10:56:16 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __ITER_HPP__
-#define __ITER_HPP__
+#ifndef __ARRAY_HPP__
+#define __ARRAY_HPP__
 
 #include <iostream>
 #include <string>
 
-template <typename T, typename F>
-void iter(T* arr, size_t size, F func) {
-	for (size_t i = 0; i < size; i++)
-		func(arr[i]);
-}
-
+template <typename T>
+class Array {
+	private:
+	
+	public:
+	
+};
 #endif
