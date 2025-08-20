6e0557bfde8c1d1f0496f05d9b88c2822e0a6ed0
AMKSA101 <abdelghaniamksa@gmail.com>
2025-08-20 10:14:38 +0100

    restored the c07/ex00/test.hpp file

--- c07/ex00/test.hpp
+++ c07/ex00/test.hpp
@@ -6,13 +6,31 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 11:52:52 by user              #+#    #+#             */
-/*   Updated: 2025/08/16 15:42:48 by user             ###   ########.fr       */
+/*   Updated: 2025/08/20 10:13:34 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __TEST_HPP__
 #define __TEST_HPP__
 
+#include <iostream>
+#include <string>
 
+template <typename T>
+void swap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+const T& min(const T& a, const T& b) {
+	return (a < b) ? a : b;
+}
+
+template <typename T>
+const T& max(const T& a, const T& b) {
+	return (a > b) ? a : b;
+}
 
 #endif
